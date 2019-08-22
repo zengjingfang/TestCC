85d4541dacfff920ea9c73dd869f08c525e65383
looperzeng <looperzeng@tencent.com>
2019-08-22 21:58:27 +0800

    新建一个工程

---
+++ Test.cpp
@@ -0,0 +1,10 @@
+//
+// Created by looperzeng on 2019/8/22.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include "Test.h"
+
+int main(){
+    printf("%d\n", 5);
+}
---
+++ Test.h
@@ -0,0 +1,8 @@
+//
+// Created by looperzeng on 2019/8/22.
+//
+
+#ifndef TESTCC_TEST_H
+#define TESTCC_TEST_H
+
+#endif //TESTCC_TEST_H
